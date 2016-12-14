78551e25e0b5dd67a61d3092e6e47bd24c9a822c
james <jamesrochabrun@gmail.com>
2016-12-13 23:52:15 -0800

    tutorial part 2

--- CollectionViewTutorial/ViewController.swift
+++ CollectionViewTutorial/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     var gridCollectionView: UICollectionView!
     var gridLayout: GridLayout!
+    var fullImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +24,19 @@ class ViewController: UIViewController {
         gridCollectionView.showsHorizontalScrollIndicator = false
         gridCollectionView.dataSource = self
         gridCollectionView.delegate = self
-        self.gridCollectionView!.register(ImageCell.self, forCellWithReuseIdentifier: "cell")
-
+        gridCollectionView!.register(ImageCell.self, forCellWithReuseIdentifier: "cell")
         self.view.addSubview(gridCollectionView)
         
+        fullImageView = UIImageView()
+        fullImageView.contentMode = .scaleAspectFit
+        fullImageView.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        fullImageView.isUserInteractionEnabled = true
+        fullImageView.alpha = 0
+        self.view.addSubview(fullImageView)
+        
+        let dismissWihtTap = UITapGestureRecognizer(target: self, action: #selector(hideFullImage))
+        fullImageView.addGestureRecognizer(dismissWihtTap)
+
     }
     
     override func viewWillLayoutSubviews() {
@@ -38,9 +48,29 @@ class ViewController: UIViewController {
         frame.origin.x = 0
         frame.origin.y = self.view.frame.minY
         gridCollectionView.frame = frame
+        fullImageView.frame = gridCollectionView.frame
     }
     
+    func showFullImage(of image:UIImage) {
+        
+        fullImageView.transform = CGAffineTransform(scaleX: 0, y: 0)
+        fullImageView.contentMode = .scaleAspectFit
+
+        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations:{[unowned self] in
+            
+            self.fullImageView.image = image
+            self.fullImageView.alpha = 1
+            self.fullImageView.transform = CGAffineTransform(scaleX: 1, y: 1)
+
+        }, completion: nil)
+    }
     
+    func hideFullImage() {
+
+        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations:{[unowned self] in
+            self.fullImageView.alpha = 0
+        }, completion: nil)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -63,6 +93,17 @@ extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
         cell.backgroundColor = #colorLiteral(red: 0.1098039216, green: 0.6392156863, blue: 0.7843137255, alpha: 1)
         cell.imageView.image = UIImage.init(named: "puppy")
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let cell = collectionView.cellForItem(at: indexPath) as! ImageCell
+        
+        if let image = cell.imageView.image {
+            self.showFullImage(of: image)
+        } else {
+            print("no photo")
+        }
     }
     
 }
